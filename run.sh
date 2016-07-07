52e8ff057976d806750f56cd491edb536e20c4a5
spacime <spacimeulim@gmail.com>
2016-07-07 11:10:00 -0700

    change the database structure

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ psql -c "create user ulim with password '3233173' createdb;" -U postgres
 psql -c "ALTER USER ulim WITH superuser;" -U postgres
 psql -c 'create database dockstore with owner = ulim;' -U postgres
 
-python fetchDataFromDockstore.py -u ulim -d dockstore -p 3233173
+python fetchDataFromDockstore.py
 python sever.py
--- sever.py
+++ sever.py
@@ -16,14 +16,15 @@ def output_container_format():
     conn_string = "host='localhost' dbname='dockstore' user='ulim' password='3233173'"
     conn = psycopg2.connect(conn_string)
     cur = conn.cursor();
-    cur.execute("SELECT name, author, description, registryId FROM gmod_tools")
+    cur.execute("SELECT toolname, author, description, url, id FROM gmod_tools")
     containers_info = cur.fetchall()
     for container_info in containers_info:
         info_dict = {}
         info_dict['name'] = container_info[0]
         info_dict['author'] = container_info[1]
         info_dict['gitUrl'] = "git@github.com:ICGC-TCGA-PanCancer/CGP-Somatic-Docker.git"
-        info_dict['path'] = container_info[3]
+        info_dict['url'] = container_info[3]
+        info_dict['path'] = container_info[4]
         output_container_arr.append(info_dict)
 
     cur.close()
@@ -38,4 +39,4 @@ def after_request(response):
   return response
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
